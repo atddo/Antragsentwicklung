f4bb244669b71d57e3d98066e94011285cfa07f4
Maximilian Brand <mbr@glstreuhand.local>
2020-04-27 12:25:18 +0200

    initial

---
+++ code/antragsgenerator.php
@@ -0,0 +1,32 @@
+<?php
+$antragssteller->name = "Verein für Antragsstellung e.V.";
+$antragssteller->gruendungsjahr = 1992;
+$antragssteller->mitglied = true;
+$antragssteller->plz = "58120";
+$antragssteller->straße = "Antragsstr. 55";
+$antragssteller->adresszusatz = "Testzusatz";
+$antragssteller->email = "info@AntragsImport.com";
+$antragssteller->telefonnummer ="02341234565797";
+$antragssteller->fax = "02341234565797";
+$antragssteller->website = "Antragsstellung.de";
+$kontaktperson->anrede = "Herr";
+$kontaktperson->name = "Test";
+$kontaktperson->vorname = "Thodor";
+$kontaktperson->funktion = "Projektkoordinator";
+$kontaktperson->email = "test@test.de";
+$kontaktperson->mobilnummer = "01711234567890";
+$kontaktperson->telefonnummer ="02341234565797";
+$vertretungsberechtigter->anrede = "Herr";
+$vertretungsberechtigter->name = "Test";
+$vertretungsberechtigter->vorname = "Thodor";
+$vertretungsberechtigter->funktion = "Projektkoordinator";
+$vertretungsberechtigter->email = "test@test.de";
+$vertretungsberechtigter->mobilnummer = "01711234567890";
+$vertretungsberechtigter->telefonnummer ="02341234565797";
+$projekt->beginn = "01.01.2020";
+$projekt->ende = "31.01.2020";
+$projekt->gesamtkosten = "1500";
+$projekt->gewuenschterBetrag = "500";
+$projekt->titel = "Tolles Projekt";
+
+?>
